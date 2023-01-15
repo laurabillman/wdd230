660a8bab17ea4829e7140738ff04ffbf93590b69
Laura Billman <laurasbillman@gmail.com>
2023-01-15 00:42:41 -0700

    added html and css for design document

---
+++ practice.js
@@ -0,0 +1,12 @@
+let hungerLevel = 7;
+
+
+// Depending on if hunger level is === as 7 or greater/lesser than 7 etc. will evaluate as true or false and print appropriate message. 
+if (hungerLevel >= 7) {
+    console.log("Time to eat!")
+
+} else {
+    console.log("We can eat later!")
+
+
+}
---
+++ snippet.js
@@ -0,0 +1,51 @@
+let x = 10;
+let y = 6;
+
+let total = x += y;
+
+console.log(total);
+
+
+
+
+let x = 10;
+let y = 6;
+
+
+console.log(x + y);
+
+console.log(x * y);
+
+console.log(x - 1);
+
+console.log()
+
+t1 = "How are you "
+t2 = "today?"
+t3 = ""
+
+console.log(t3 = t1 + t2)
+console.log(t1 +=t2)
+
+console.log(x == y)
+
+console.log(x < 20)
+
+console.log(y<10)
+
+console.log(x !== 5)
+
+console.log(y !== x)
+
+console.log(x !== 10)
+
+console.log(x >)
+
+//check to see if a value is less than 10 and greater than 0
+console.log(x < 10)
+console.log(x=y)
+
+
+
+// increment the value in a variable by 3
+console.log()
