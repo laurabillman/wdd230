ec7034f2751dd6fd2539a94f75b622f04e99d502
Laura Billman <laurasbillman@gmail.com>
2023-02-03 20:35:35 -0700

    added html and css and js banner

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -10,3 +10,39 @@ x.onclick = toggleMenu;
 
 const todaysdate = new Date();
 console.log(todaysdate);
+
+
+let now = new Date();
+var hrs = now.getHours();
+var ins = now.getMinutes();
+let secs = now.getSeconds();
+
+
+
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let day = weekday[now.getDay()];
+console.log(day);
+
+if (day == "Friday") {
+    let message = ("Come join us for the Chamber meet and greet Wednesday at 7:00p.m.");
+    console.log(message);
+    document.getElementById("message").textContent = message;  
+
+    }
+else if (day == "Tuesday") {
+    let message = ("Come join us for the Chamber meet and greet Wednesday at 7:00p.m.");
+    console.log(message);
+    document.getElementById("message").textContent = message;  
+
+}
+
+
+   
