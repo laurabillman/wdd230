c9ebde03b3429f09dbc6d15cdbe58970b1dad4f1
Laura Billman <laurasbillman@gmail.com>
2023-02-11 23:27:43 -0700

    optimize images hero small medium large

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -44,5 +44,40 @@ else if (day == "Tuesday") {
 
 }
 
+let temp = parseInt(temperature.textContent);
 
-   
+
+console.log(temp);
+
+let speed = parseInt(windSpeed.textContent);
+
+console.log(speed);
+
+calculateWC(temp, speed);
+
+
+
+
+/* **************************************
+* Calculates the wind chill temperature
+*************************************** */
+
+function calculateWC(temperature, speed) {
+    let windChill = document.getElementById("windChill");
+
+    let wc = 35.74 + (0.6215 * temp) -(35.75 * Math.pow(speed, 0.16)) + (0.4275*temp) * Math.pow(speed, 0.16);
+    console.log(wc);
+    
+    //Round the answer down to integer
+    wc = Math.floor(wc);
+
+    //If chill is greater than temp, return the temp
+    wc = (wc >temp) ? temp : wc;
+
+    //Display the windchill
+    console.log(wc);
+
+    //wc = "feels like "wc+"°F";
+    windChill.innerHTML = wc;
+
+}
