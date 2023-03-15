efcee61d2bd6fc737ee4ac92ea5f4b76100235fc
Laura Billman <laurasbillman@gmail.com>
2023-03-14 23:13:19 -0600

    added css and htmlfor contact page

---
+++ chamber/contact.js
@@ -0,0 +1,17 @@
+function dateHidden() {
+    let currentDate = new Date();
+    let cDay = currentDate.getDate();
+    let cMonth = currentDate.getMonth() + 1;
+    let cYear = currentDate.getFullYear();
+    console.log(cDay);
+    console.log(cMonth);
+    console.log(cYear);
+
+    let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+    console.log(time);
+
+    document.getElementById("hiddenDate").value = currentDate;
+    console.log(currentDate, time); 
+}
+
+document.getElementById("submitButton").addEventListener("click", dateHidden);
