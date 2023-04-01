b818bd055ef838d1df0f88ebfeadab40eb3b4f64
Laura Billman <laurasbillman@gmail.com>
2023-04-01 01:13:52 -0600

    worked on html and css large view

---
+++ bountiful-foods/scripts/main.js
@@ -0,0 +1,48 @@
+function toggleMenu() {
+    document.getElementById("top-nav").classList.toggle("open")
+    document.getElementById("hamburger-btn").classList.toggle("open")
+}
+
+const x = document.getElementById("hamburger-btn");
+x.onclick = toggleMenu;
+
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach(img => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
+
+
+
+
+
+
--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,22 +1,42 @@
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
+const humidity = document.querySelector("#humidity");
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Syracuse,US&units=imperial&APPID=93e08f2f8e84447a287da3181b88089b";
+const captionDesc = document.querySelector('#description1');
+const apiURLToday = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad,US&units=imperial&APPID=e6781c90308de4dbed6f210d3b08b464";
 
-let weatherData;
+const apiURL3Day = "https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&units=imperial&appid=e6781c90308de4dbed6f210d3b08b464";//
+let weatherDataToday;
+let weatherData3Day;
 
-apiFetch();
+apiFetchCurrent();
+apiFetch3Day();
 
 //function to fetch api. If response is okay sets data to await response 
-async function apiFetch() {
+async function apiFetchCurrent() {
     try {
-        const response = await fetch(apiURL);
+        const response = await fetch(apiURLToday);
         if (response.ok) {
-           weatherData = await response.json();
-            console.log(weatherData); // this is for testing the call
-            displayResults(weatherData);
-            calculateWC();
+            weatherDataToday = await response.json();
+            console.log(weatherDataToday); // this is for testing the call
+            displayResults(weatherDataToday);
+
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function apiFetch3Day() {
+    try {
+        const response = await fetch(apiURL3Day);
+        if (response.ok) {
+            weatherData3Day = await response.json();
+            console.log(weatherData3Day); // this is for testing the call
+            display3Day(weatherData3Day);
+
         } else {
             throw Error(await response.text());
         }
@@ -33,20 +53,67 @@ function capitalize(s) {
 function displayResults(weatherData) {
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description.split(" ").map(capitalize).join(" ");
-    
+
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    humidity.innerHTML = weatherData.main.humidity;
     weatherIcon.setAttribute("src", iconsrc);
     weatherIcon.setAttribute("alt", desc);
     captionDesc.textContent = desc;
 }
 
+function display3Day(weatherData2) {
+    let daysForecast = [];
+    for (let i = 0; i < weatherData2.list.length; i++) {
+        const date = new Date(weatherData2.list[i].dt_txt);
+        const dayOfMonth = date.getDate();
+        if (i == 0) {
+            daysForecast.push({ date: date, forecast: weatherData2.list[i] });
+        }
+        else if (daysForecast[daysForecast.length - 1].date.getDate() != dayOfMonth && daysForecast.length < 3) {
+            daysForecast.push({ date: date, forecast: weatherData2.list[i] });
+        }
+    
+ 
+
+    }
+    
+    //daysForecast.shift();
+    
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
+    document.querySelector("#day2").innerHTML = `${daysOfTheWeek[daysForecast[0].date.getDay()]} forecast`;
+    document.querySelector("#day2-temp").innerHTML = `<strong>${daysForecast[0].forecast.main.temp.toFixed(0)}</strong>`;
+    document.querySelector("#description2").innerHTML = daysForecast[0].forecast.weather[0].description;
+    document.querySelector("#humidity2").innerHTML = daysForecast[0].forecast.main.humidity; 
+    document.querySelector("#weather-icon2").setAttribute("src", `https://openweathermap.org/img/w/${daysForecast[0].forecast.weather[0].icon}.png`);
+    document.querySelector("#weather-icon2").setAttribute("alt", daysForecast[0].forecast.weather[0].description.split(" ").map(capitalize).join(" "));
+   
+    console.log(daysForecast);
+
+    document.querySelector("#day3").innerHTML = `${daysOfTheWeek[daysForecast[1].date.getDay()]} forecast`;
+    document.querySelector("#day3-temp").innerHTML = `<strong>${daysForecast[1].forecast.main.temp.toFixed(0)}</strong>`;
+    document.querySelector("#description3").innerHTML = daysForecast[1].forecast.weather[0].description;
+    document.querySelector("#humidity3").innerHTML = daysForecast[1].forecast.main.humidity; 
+    document.querySelector("#weather-icon3").setAttribute("src", `https://openweathermap.org/img/w/${daysForecast[1].forecast.weather[0].icon}.png`);
+    document.querySelector("#weather-icon3").setAttribute("alt", daysForecast[1].forecast.weather[0].description.split(" ").map(capitalize).join(" "));
+   
+
+    document.querySelector("#day4").innerHTML = `${daysOfTheWeek[daysForecast[2].date.getDay()]} Forecast`;
+    document.querySelector("#day4-temp").innerHTML = `<strong>${daysForecast[2].forecast.main.temp.toFixed(0)}</strong>`;
+    document.querySelector("#description4").innerHTML = daysForecast[2].forecast.weather[0].description;
+    document.querySelector("#humidity4").innerHTML = daysForecast[2].forecast.main.humidity; 
+    document.querySelector("#weather-icon4").setAttribute("src", `https://openweathermap.org/img/w/${daysForecast[2].forecast.weather[0].icon}.png`);
+    document.querySelector("#weather-icon4").setAttribute("alt", daysForecast[2].forecast.weather[0].description.split(" ").map(capitalize).join(" "));
+   
+
+}
 /* **************************************
 * Calculates the wind chill temperature
 *************************************** */
 
 
 
-function calculateWC() {
+/*function calculateWC() {
     //debugger
     let windSpeed = document.getElementById("windSpeed");
     let windChill = document.getElementById("windChill");
@@ -71,4 +138,4 @@ function calculateWC() {
     windChill.innerHTML = wc+"°F";
     windSpeed.innerHTML = weatherData.wind.speed+" km/h";
 
-}
+}*/
